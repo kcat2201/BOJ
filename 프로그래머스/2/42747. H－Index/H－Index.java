befa2a88496fdd6798d677bf8b574df20179686a
kcat2201 <kcat2201@gmail.com>
2024-08-02 14:30:54 +0900

    [level 2] Title: H-Index, Time: 2.47 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,28 +1,26 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-               List<Integer> list = new ArrayList<>();
+            int answer = 0;
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < citations.length; i++) {
             list.add(citations[i]);
         }
-
         Collections.sort(list, Collections.reverseOrder());
-        int answer = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) <= i ) {
-                answer = i;
+        int index = (list.size() - 1) / 2;
+        int stand = list.get(index);
+
+        while (answer >= 0) {
+            long count = list.stream().filter(c -> c >= stand).count();
+            long count2 = list.stream().filter(c -> c <= stand).count();
+            if (count >= stand && count2 <= stand) {
+                answer = stand;
                 break;
+            } else {
+                answer -= 1;
             }
         }
-        if (answer == 0) {
-            answer = list.size();
-        }
-        
-         if (list.get(0) == 0)
-            return 0;
-        return answer;
+        return answer+1;
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,6 +1,6 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?itm_content=course14743) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 02일 14:30:15
+2024년 08월 02일 14:30:52
 
 ### 문제 설명
 
